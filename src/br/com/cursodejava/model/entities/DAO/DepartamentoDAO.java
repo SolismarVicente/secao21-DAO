fa4a6d87cf098d58caee50d39e8f4e69bce7bf47
SolismarVicente <solismarvicente@gmail.com>
2021-01-07 23:50:53 -0300

    Interfaces DepartamentoDAO e VendedorDAO

---
+++ src/br/com/cursodejava/model/entities/DAO/DepartamentoDAO.java
@@ -0,0 +1,16 @@
+package br.com.cursodejava.model.entities.DAO;
+
+import java.util.List;
+
+import br.com.cursodejava.model.entities.Departamento;
+
+public interface DepartamentoDAO {
+
+	public void inserir(Departamento departamento);
+	public void alterar(Departamento departamento);
+	public void ExcluirPorCodigo(Integer codigo);
+	public Departamento buscarPorCodigo(Integer codigo);
+	public List<Departamento> listarTodos();
+	
+	
+}
---
+++ src/br/com/cursodejava/model/entities/DAO/VendedorDAO.java
@@ -0,0 +1,16 @@
+package br.com.cursodejava.model.entities.DAO;
+
+import java.util.List;
+
+import br.com.cursodejava.model.entities.Vendedor;
+
+public interface VendedorDAO {
+
+	public void inserir(Vendedor vendedor);
+	public void alterar(Vendedor vendedor);
+	public void ExcluirPorCodigo(Integer codigo);
+	public Vendedor buscarPorCodigo(Integer codigo);
+	public List<Vendedor> listarTodos();
+	
+	
+}
